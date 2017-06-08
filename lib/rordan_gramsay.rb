e9b81733c93e9d0c845813765dc885422c428431
David Alexander <david.alexander@salesforce.com>
2017-06-08 12:39:46 -0400

    Noisy gems are noisy

--- lib/rordan_gramsay.rb
+++ lib/rordan_gramsay.rb
@@ -1,4 +1,11 @@
 require_relative 'rordan_gramsay/version'
 
 # :nodoc:
-module RordanGramsay; end
+module RordanGramsay
+  def self.silence_warnings
+    original_verbosity = $VERBOSE
+    $VERBOSE = nil
+    yield
+    $VERBOSE = original_verbosity
+  end
+end
